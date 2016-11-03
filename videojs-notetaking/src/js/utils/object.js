708a0a8fd2b31758a0cb59f4e38d06c6d34c4e2b
Shota Makino <shota.makino@yahoo.com>
2016-11-03 16:06:54 -0700

    Changed directory structure

--- videojs-notetaking/src/js/utils/object.js
+++
@@ -1,8 +0,0 @@
-export function isEmpty(obj) {
-    for(var prop in obj) {
-        if(obj.hasOwnProperty(prop))
-            return false;
-    }
-
-    return true && JSON.stringify(obj) === JSON.stringify({});
-}
